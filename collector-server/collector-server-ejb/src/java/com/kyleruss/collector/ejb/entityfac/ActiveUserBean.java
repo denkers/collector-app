cea29dc3898fa95bf1bc4ac24c1362fe9289211c
denkers <kyle.business@inbox.com>
2016-05-13 16:58:24 +1200

    Added singletonbean ejb.entityfac.ActiveUserBean

---
+++ collector-server/collector-server-ejb/src/java/com/kyleruss/collector/ejb/entityfac/ActiveUserBean.java
@@ -0,0 +1,38 @@
+//=========================================
+//  KYLE RUSSELL
+//  AUT UNIVERSITY 2016
+//  https://github.com/denkers/jforum
+//=========================================
+
+package com.kyleruss.collector.ejb.entityfac;
+
+import com.kyleruss.collector.ejb.entity.Users;
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+
+/**
+ * A singleton bean that's used to maintain 
+ * the active user entity of the client
+ */
+
+@Singleton
+@LocalBean
+public class ActiveUserBean
+{
+    private Users activeUser;
+    
+    public void setActiveUser(Users activeUser)
+    {
+        this.activeUser =   activeUser;
+    }
+    
+    public Users getActiveUser()
+    {
+        return activeUser;
+    }
+    
+    public boolean isActive()
+    {
+        return activeUser != null;
+    }
+}
