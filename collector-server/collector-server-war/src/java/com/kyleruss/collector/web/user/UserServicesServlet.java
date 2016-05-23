1f92f9b0a310edd078dd94e2999130416b656758
denkers <kyle.business@inbox.com>
2016-05-23 14:12:00 +1200

    Added json responses to user service methods in web.user.UserServicesServlet

--- collector-server/collector-server-war/src/java/com/kyleruss/collector/web/user/UserServicesServlet.java
+++ collector-server/collector-server-war/src/java/com/kyleruss/collector/web/user/UserServicesServlet.java
@@ -9,8 +9,9 @@ package com.kyleruss.collector.web.user;
 import com.kyleruss.collector.ejb.entity.Users;
 import com.kyleruss.collector.ejb.entityfac.ActiveUserBean;
 import com.kyleruss.collector.ejb.entityfac.UsersFacade;
+import com.kyleruss.collector.web.util.ActionResponse;
+import com.kyleruss.collector.web.util.ServletUtils;
 import java.io.IOException;
-import java.util.AbstractMap;
 import java.util.Map;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
@@ -19,7 +20,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "UserServicesServlet", urlPatterns = {"/user/login", "/user/register"})
+@WebServlet(name = "UserServicesServlet", urlPatterns = {"/user/login", "/user/register", "/user/logout"})
 public class UserServicesServlet extends HttpServlet
 {
     @EJB
@@ -31,6 +32,19 @@ public class UserServicesServlet extends HttpServlet
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
     {
+        String path =   request.getServletPath();
+        
+        if(path.equals("/user/login"))
+            postLogin(request, response);
+        
+        else if(path.equals("/user/register"))
+            postRegister(request, response);
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        getLogout(request, response);
     }
     
     private void postLogin(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
@@ -42,9 +56,8 @@ public class UserServicesServlet extends HttpServlet
         request.getSession().setAttribute("activeUser", attempt.getKey());
         activeUserBean.setActiveUser(attempt.getKey());
         
-        Map.Entry<Boolean, String> result   =   new AbstractMap.SimpleEntry(attempt.getKey() != null, attempt.getValue());   
-        //send result
-        
+        ActionResponse result   =   new ActionResponse(attempt.getValue(), attempt.getKey() != null);
+        ServletUtils.jsonResponse(response, result);
     }
     
     private void postRegister(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
@@ -55,7 +68,7 @@ public class UserServicesServlet extends HttpServlet
         String email        =   request.getParameter("register_email");
         
         Map.Entry<Boolean, String> result   =   usersBean.createUserAccount(username, password, rePassword, email);
-        //send result
+        ServletUtils.jsonResponse(response, new ActionResponse(result.getValue(), result.getKey()));
     }
     
     private void getLogout(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
@@ -64,9 +77,10 @@ public class UserServicesServlet extends HttpServlet
         {
             activeUserBean.setActiveUser(null);
             request.getSession().removeAttribute("activeUser");
-            //send result
+            ServletUtils.jsonResponse(response, new ActionResponse("Successfully logged out", true));
         }
 
-        //send failed result
+        else
+            ServletUtils.jsonResponse(response, new ActionResponse("You are not logged in", false));
     }
 }
