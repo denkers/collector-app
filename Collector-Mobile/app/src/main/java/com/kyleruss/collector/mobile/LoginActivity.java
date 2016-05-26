51168b70ccc7ce5617db7327815649c97413424f
kyle <kyle.business@inbox.com>
2016-05-26 22:25:36 +1200

    Added internet permission, changed login icons and layout

--- Collector-Mobile/app/src/main/java/com/kyleruss/collector/mobile/LoginActivity.java
+++ Collector-Mobile/app/src/main/java/com/kyleruss/collector/mobile/LoginActivity.java
@@ -3,6 +3,9 @@ package com.kyleruss.collector.mobile;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity
 {
@@ -11,6 +14,15 @@ public class LoginActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
 
+    public void attemptLogin(View v)
+    {
+        TextView usernameView   =   (TextView) findViewById(R.id.userField);
+        TextView passwordView   =   (TextView) findViewById(R.id.passField);
+        String username         =   usernameView.getText().toString();
+        String password         =   passwordView.getText().toString();
+
+        Toast.makeText(getApplicationContext(), "username: " + username + " pass: " + password, Toast.LENGTH_SHORT).show();
     }
 }
