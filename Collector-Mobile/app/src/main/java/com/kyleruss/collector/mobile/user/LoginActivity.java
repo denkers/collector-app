ceda9859ce0d348992899127a3595b3312752460
kyle <kyle.business@inbox.com>
2016-05-28 21:41:32 +1200

    Moved LoginActivity, RegisterActivity -> user.LoginActivity, user.RegisterActivity

---
+++ Collector-Mobile/app/src/main/java/com/kyleruss/collector/mobile/user/LoginActivity.java
@@ -0,0 +1,97 @@
+package com.kyleruss.collector.mobile.user;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.kyleruss.collector.mobile.Consts;
+import com.kyleruss.collector.mobile.R;
+import com.kyleruss.collector.mobile.comms.HTTPAsync;
+import com.kyleruss.collector.mobile.comms.ServiceRequest;
+import com.kyleruss.collector.mobile.comms.ServiceResponse;
+
+public class LoginActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initSavedCredentials();
+    }
+
+    public void attemptLogin(View v)
+    {
+        TextView usernameView   =   (TextView) findViewById(R.id.userField);
+        TextView passwordView   =   (TextView) findViewById(R.id.passField);
+        String username         =   usernameView.getText().toString();
+        String password         =   passwordView.getText().toString();
+
+
+        ServiceRequest request  =   new ServiceRequest(Consts.ROOT_URL + "user/login", false);
+        request.addParam("login_username", username);
+        request.addParam("login_password", password);
+        LoginServicer servicer  =   new LoginServicer();
+        servicer.execute(request);
+    }
+
+    public void showRegister(View v)
+    {
+        Intent intent   =   new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void initSavedCredentials()
+    {
+        String[] savedCredentials   =   UserCredentialsManager.getSavedCredentials(this);
+        String username             =   savedCredentials[0];
+        String password             =   savedCredentials[1];
+
+        ((EditText) findViewById(R.id.userField)).setText(username);
+        ((EditText) findViewById(R.id.passField)).setText(password);
+
+        if(!username.equals("") && !password.equals(""))
+            ((CheckBox) findViewById(R.id.rememberPassCheck)).setChecked(true);
+    }
+
+    public void saveCredentials()
+    {
+        if(((CheckBox) findViewById(R.id.rememberPassCheck)).isChecked())
+        {
+            String username     =   ((EditText) findViewById(R.id.userField)).getText().toString();
+            String password     =   ((EditText) findViewById(R.id.passField)).getText().toString();
+            UserCredentialsManager.saveCredentials(username, password, this);
+        }
+
+        else UserCredentialsManager.removeCredentials(this);
+    }
+
+    private class LoginServicer extends HTTPAsync
+    {
+        @Override
+        protected void onPreExecute()
+        {
+            super.onPreExecute();
+            ImageView loginControl  =   (ImageView) findViewById(R.id.loginBtn);
+            showServicingSpinner(loginControl);
+        }
+
+        @Override
+        protected void onPostExecute(String response)
+        {
+            ServiceResponse serviceResponse =   getServiceResponse(response);
+            System.out.println("status: " + serviceResponse.getStatus() + " message: " + serviceResponse.getMessage());
+            if(serviceResponse.getStatus())
+                saveCredentials();
+
+            ImageView loginControl      =   (ImageView) findViewById(R.id.loginBtn);
+            hideServicingSpinner(loginControl, R.drawable.loginbtn);
+            serviceResponse.showToastResponse(LoginActivity.this);
+        }
+    }
+}
---
+++ Collector-Mobile/app/src/main/java/com/kyleruss/collector/mobile/user/RegisterActivity.java
@@ -0,0 +1,61 @@
+package com.kyleruss.collector.mobile.user;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.kyleruss.collector.mobile.Consts;
+import com.kyleruss.collector.mobile.R;
+import com.kyleruss.collector.mobile.comms.HTTPAsync;
+import com.kyleruss.collector.mobile.comms.ServiceRequest;
+import com.kyleruss.collector.mobile.comms.ServiceResponse;
+
+public class RegisterActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        setTitle("Registration");
+    }
+
+    public void attemptRegister(View v)
+    {
+        String username     =   ((EditText) findViewById(R.id.regUsernameField)).getText().toString();
+        String password     =   ((EditText) findViewById(R.id.regPasswordField)).getText().toString();
+        String rePassword   =   ((EditText) findViewById(R.id.regRePasswordField)).getText().toString();
+        String email        =   ((EditText) findViewById(R.id.regEmailField)).getText().toString();
+
+        ServiceRequest request  =   new ServiceRequest(Consts.ROOT_URL + "user/register", false);
+        request.addParam("register_username", username);
+        request.addParam("register_password", password);
+        request.addParam("register_re_password", rePassword);
+        request.addParam("register_email", email);
+
+        RegisterServicer servicer    =   new RegisterServicer();
+        servicer.execute(request);
+    }
+
+    private class RegisterServicer extends HTTPAsync
+    {
+        @Override
+        protected void onPreExecute()
+        {
+            ImageView registerControl   =   (ImageView) findViewById(R.id.registerAttemptBtn);
+            showServicingSpinner(registerControl);
+        }
+
+        @Override
+        protected void onPostExecute(String response)
+        {
+            ServiceResponse serviceResponse =   getServiceResponse(response);
+            ImageView registerControl       =   (ImageView) findViewById(R.id.registerAttemptBtn);
+            hideServicingSpinner(registerControl, R.drawable.registerbtn);
+            serviceResponse.showToastResponse(RegisterActivity.this);
+            finish();
+        }
+    }
+}
