c5cf39a523242ecb8378d61218e402c72acd02f5
kyle <kyle.business@inbox.com>
2016-05-30 21:38:43 +1200

    Added record.User

---
+++ Collector-Mobile/app/src/main/java/com/kyleruss/collector/mobile/record/User.java
@@ -0,0 +1,73 @@
+package com.kyleruss.collector.mobile.record;
+
+import java.util.Date;
+
+public class User
+{
+    private String username;
+    private String password;
+    private String picture;
+    private String email;
+    private String country;
+    private Date registerDate;
+
+    public Date getRegisterDate()
+    {
+        return registerDate;
+    }
+
+    public void setRegisterDate(Date registerDate)
+    {
+        this.registerDate = registerDate;
+    }
+
+    public String getCountry()
+    {
+        return country;
+    }
+
+    public void setCountry(String country)
+    {
+        this.country = country;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getPicture()
+    {
+        return picture;
+    }
+
+    public void setPicture(String picture)
+    {
+        this.picture = picture;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+}
