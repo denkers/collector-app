324ea786c44939da3f32bc1b690453369758bb34
kyle <kyle.business@inbox.com>
2016-06-01 03:44:15 +1200

    Added saving settings functionality

--- Collector-Mobile/app/src/main/java/com/kyleruss/collector/mobile/base/SettingsFragment.java
+++ Collector-Mobile/app/src/main/java/com/kyleruss/collector/mobile/base/SettingsFragment.java
@@ -13,12 +13,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.kyleruss.collector.mobile.Consts;
 import com.kyleruss.collector.mobile.R;
+import com.kyleruss.collector.mobile.comms.HTTPAsync;
+import com.kyleruss.collector.mobile.comms.ServiceRequest;
+import com.kyleruss.collector.mobile.comms.ServiceResponse;
 import com.kyleruss.collector.mobile.record.User;
 import com.kyleruss.collector.mobile.user.ActiveUser;
 
-public class SettingsFragment extends Fragment
+public class SettingsFragment extends Fragment implements View.OnClickListener
 {
     public SettingsFragment() {}
 
@@ -36,6 +41,61 @@ public class SettingsFragment extends Fragment
             ((EditText) view.findViewById(R.id.settingsPictureField)).setText(user.getPicture());
         }
 
+        ImageView saveControl   =   (ImageView) view.findViewById(R.id.settingsSaveBtn);
+        saveControl.setOnClickListener(this);
+
         return view;
+    }
+
+    public void saveSettings(View v)
+    {
+        View view       =   getView();
+        String pass     =   ((EditText) view.findViewById(R.id.settingsPasswordField)).getText().toString();
+        String email    =   ((EditText) view.findViewById(R.id.settingsEmailField)).getText().toString();
+        String picture  =   ((EditText) view.findViewById(R.id.settingsPictureField)).getText().toString();
+
+        ServiceRequest request  =   new ServiceRequest(Consts.ROOT_URL + "/user/profile/settings/save", false);
+        request.addParam("settings_password", pass);
+        request.addParam("settings_email", email);
+        request.addParam("settings_picture", picture);
+
+        SettingsServicer servicer   =   new SettingsServicer();
+        servicer.execute(request);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        if(v.getId() == R.id.settingsSaveBtn)
+            saveSettings(v);
+    }
+
+    private class SettingsServicer extends HTTPAsync
+    {
+        @Override
+        protected void onPreExecute()
+        {
+            super.onPreExecute();
+            ImageView loginControl  =   (ImageView) getView().findViewById(R.id.settingsSaveBtn);
+            showServicingSpinner(loginControl);
+        }
+
+        @Override
+        protected void onPostExecute(String response)
+        {
+            ServiceResponse serviceResponse =   getServiceResponse(response);
+
+            ImageView saveControl  =   (ImageView) getView().findViewById(R.id.settingsSaveBtn);
+            hideServicingSpinner(saveControl, R.drawable.savebtn);
+            serviceResponse.showToastResponse(getActivity());
+
+            if(serviceResponse.getStatus())
+            {
+                View view       =   getView();
+                String pass     =   ((EditText) view.findViewById(R.id.settingsPasswordField)).getText().toString();
+                String email    =   ((EditText) view.findViewById(R.id.settingsEmailField)).getText().toString();
+                String picture  =   ((EditText) view.findViewById(R.id.settingsPictureField)).getText().toString();
+            }
+        }
     }
 }
